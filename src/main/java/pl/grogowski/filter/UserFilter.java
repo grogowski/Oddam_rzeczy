8d2546047105e93477d02c0e52bb8e54503ea539
Grzegorz Rogowski <rogowski.g@gmail.com>
2019-03-03 21:51:55 +0100

    added AdminFilter and UserFilter for access restriction

---
+++ src/main/java/pl/grogowski/filter/UserFilter.java
@@ -0,0 +1,35 @@
+package pl.grogowski.filter;
+
+import pl.grogowski.model.User;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter("/user/*")
+public class UserFilter implements Filter {
+    public void destroy() {
+    }
+
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+        HttpSession session = ((HttpServletRequest) req).getSession();
+        if (session.getAttribute("user") == null) {
+            ((HttpServletResponse) resp).sendRedirect("/");
+        } else {
+            User user = (User) session.getAttribute("user");
+            if (user.getAdmin()) {
+                ((HttpServletResponse) resp).sendRedirect("/admin/main");
+            }
+            chain.doFilter(req, resp);
+        }
+
+    }
+
+    public void init(FilterConfig config) throws ServletException {
+
+    }
+
+}
